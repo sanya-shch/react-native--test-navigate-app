6948ed66458819438c72b965bc3c26789765507a
sanya-shch <shchipkov.sanya@ukr.net>
2020-01-04 16:31:41 +0200

    simple navigate

--- App.js
+++ App.js
@@ -1,11 +1,45 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from "react-native";
+import { NavigationNativeContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
+
+const HomeScreen = ({ navigation }) => (
+  <View style={styles.container}>
+      <Text>HomeScreen</Text>
+      <Button
+          title="Go To Settings Screen"
+          onPress={() => navigation.navigate("Settings")}
+      />
+  </View>
+);
+
+const SettingsScreen = ({ navigation }) => (
+  <View style={styles.container}>
+      <Text>SettingsScreen</Text>
+      <Button
+          title="Go To Home Screen"
+          onPress={() => navigation.goBack()}
+      />
+  </View>
+);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+      <NavigationNativeContainer>
+          <Stack.Navigator>
+              <Stack.Screen
+                  options={{ title: "Home Screen" }}
+                  name="Home"
+                  component={HomeScreen}
+              />
+              <Stack.Screen
+                  name="Settings"
+                  component={SettingsScreen}
+              />
+          </Stack.Navigator>
+      </NavigationNativeContainer>
   );
 }
 
